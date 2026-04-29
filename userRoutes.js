b5cfae06a76be6f6ff1e701668280c4b0b603ff7
Tanu Mangal <tanu.mangal_cs24@gla.ac.in>
2026-04-29 10:35:45 +0530

    Add files via upload

---
+++ userRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const protect = require("../middlewares/authMiddleware");
+
+// Protected route
+router.get("/profile", protect, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user,
+  });
+});
+
+module.exports = router;
